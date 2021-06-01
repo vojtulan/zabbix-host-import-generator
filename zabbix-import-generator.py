338c89478ca9d114267fec3e998d4c006e3ba176
vojtulan <52625189+vojtulan@users.noreply.github.com>
2021-06-01 17:15:09 +0200

    Update zabbix-import-generator.py

--- zabbix-import-generator.py
+++ zabbix-import-generator.py
@@ -3,30 +3,42 @@
 import json
 import copy
 
+print("Script has started")
+
 with open("host_template.json", "r") as f:
      data = json.load(f)
-with open("hosts.txt") as g:
-     hostsPerLine =  g.read().splitlines()
-with open("description.txt") as h:
+with open("names.txt") as g:
+     names =  g.read().splitlines()
+with open("descriptions.txt") as h:
      descriptions = h.read().splitlines()
-with open("ip.txt") as j:
+with open("ips.txt") as j:
      ips = j.read().splitlines()
-     result = ""
-     i = 0
-     output = ""
-     for x in hostsPerLine:
-        temp = copy.deepcopy(data)
-        nowHost = hostsPerLine[i]
-        nowDesc = descriptions[i]
-        nowIp = ips[i]
-        temp['host'] = nowHost
-        temp['name'] = nowHost
-        temp['interfaces'][0]['ip'] = nowIp
-        temp['description'] = nowDesc
-        result = result + json.dumps(temp) + ", "
-        i = i + 1
 
-result
-with open("final.json", "w") as json_out:
-   json_out.write(a)
-   print(json_out)
+numHost = sum(1 for line in open('names.txt'))
+numDesc = sum(1 for line in open('descriptions.txt'))
+numIp = sum(1 for line in open('ips.txt'))
+
+print("Number of names: ", numHost)
+print("Number of descriptions: ", numDesc)
+print("Number of ips: ", numIp)
+
+result = ""
+output = ""
+i = 0
+for x in names:
+   temp = copy.deepcopy(data)
+   host = names[i]
+   desc = descriptions[i]
+   ip = ips[i]
+   temp['host'] = host
+   temp['name'] = host
+   temp['interfaces'][0]['ip'] = ip
+   temp['description'] = desc
+   result = result + json.dumps(temp) + ", "
+   i = i + 1
+
+result = result[:-2]   #remove space and last  comma
+with open("import.json", "w") as json_out:
+   json_out.write(result)
+
+print("Script has finished")
